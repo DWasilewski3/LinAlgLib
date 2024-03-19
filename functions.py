7de5e47a6bff004648d592ae865dba35b2cc33b4
David Wasilewski <wasile19@msu.edu>
2024-03-19 18:02:30 -0400

    Added dependency libraries

--- functions.py
+++ functions.py
@@ -1,2 +1,11 @@
 def test ():
     print("Hello World")
+
+#If contributing, please add boolean 'showWork' and show steps to calculate if true
+
+import numpy as np
+import sympy as sym  
+import scipy as sp
+
+def inverse():
+    return np.linalg
--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DWAS-LinAlg',
+    name='LinAlgLib',
     version='0.1',
     packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'sympy',
+        'scipy'
+    ],
 )
