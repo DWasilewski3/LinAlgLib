5f99512d446f7deb65ccc9e46ee31708ccf36bb6
David Wasilewski <wasile19@msu.edu>
2024-04-03 17:56:04 -0400

    Update functions.py

--- LinAlgLib/functions.py
+++ LinAlgLib/functions.py
@@ -559,11 +559,3 @@ def gram_schmidt(vector_list):
     basis_approx = [b.evalf() for b in basis]
     
     return basis_approx
-
-
-
-
-
-
-vectors = [[-1, 3, 1, 1], [6, -8, -2, -4], [6, 3, 6, -3]]
-print(gram_schmidt(vectors))
