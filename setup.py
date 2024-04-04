86304b254b821a7704e64b29dd418bf4a4775977
David Wasilewski <wasile19@msu.edu>
2024-04-04 13:32:12 -0400

    New dist 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='LinAlgLib',
-    version='0.1.2',
+    version='0.1.3',
     author='David_Wasilewski',
     packages=find_packages(),
     py_modules=['functions'],
